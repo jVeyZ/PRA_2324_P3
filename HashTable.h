3c6fac79b19ce32d41aca21d0e3ca342cac337a4
Javier Veyrat <146321233+jVeyZ@users.noreply.github.com>
2024-11-14 18:52:08 +0100

    Actualizado Makefile y añadido código de test de la clase HashTable

--- HashTable.h
+++ HashTable.h
@@ -1,75 +1,83 @@
 #ifndef HASHTABLE_H
 #define HASHTABLE_H
 
-#include <ostream>
-#include <stdexcept>
 #include "Dict.h"
 #include "TableEntry.h"
-
-#include "../act1/ListLinked.h" 
+#include <ostream>
+#include <stdexcept>
+#include "../act1/ListLinked.h"
 
 template <typename V>
-class HashTable: public Dict<V> {
+class HashTable : public Dict<V> {
+private:
+    int n; // Number of entries in the table
+    int max; // Maximum size of the hash table
+    ListLinked<TableEntry<V> > *table; // Array of linked lists for table entries
 
-    private:
-        int n;
-        int max;
-        ListLinked<TableEntry<V>>* table;
-        int h(std::string key){
-            int temp = 0;
-            int i = 0;
-            while (i < key.size()) {
-                temp += static_cast<int>(key[i]);
-                i++;}
-            return temp%max;
+    int h(const std::string &key) const {
+        int hash = 0;
+        for (size_t i = 0; i < key.size(); ++i) {
+            hash += static_cast<int>(key[i]);
         }
+        return hash % max;
+    }
 
-    public:
-        HashTable(int size):n(0),max(size){
-            this->table = new ListLinked<TableEntry<V>>[size];
+public:
+    HashTable(int size) : n(0), max(size) {
+        this->table = new ListLinked<TableEntry<V> >[max];
+    }
+
+    ~HashTable() { delete[] table; }
+
+    int capacity() const { return max; }
+
+    friend std::ostream &operator<<(std::ostream &out, const HashTable<V> &th) {
+        for (int i = 0; i < th.max; i++) {
+            out << i << ": " << th.table[i] << std::endl;
         }
-        ~HashTable(){
-            delete table;
+        return out;
+    }
+
+    V operator[](const std::string &key) {
+        int pos = h(key);
+        int idx = table[pos].search(TableEntry<V>(key));
+        if (idx == -1) {
+            throw std::runtime_error("Key not found.");
         }
-        int capacity(){
-            return max;
+        return table[pos].get(idx).value;
+    }
+
+    void insert(const std::string &key, V value) override{
+        int pos = h(key);
+        if (table[pos].search(TableEntry<V>(key)) != -1) {
+            throw std::runtime_error("Key already exists.");
         }
-        friend std::ostream& operator<<(std::ostream &out, const HashTable<V> &th){
-            for (int i = 0; i < th.capacity(); i++){out << th.table[i].key << "->" << th.table[i].value << std::endl;}
-            return out;
-        }
-        V operator[](std::string key){
-            search(key);
-        }
-        
-        void insert(std::string key, V value) override {
-            for (V node : table[max]) {
-                if (node.key == key) {
-                    throw std::runtime_error("Key already exists.");
-                }
-            }
-            ListLinked<TableEntry<V>>::insert(n,value);
-            n++;
-        }
-        V search(std::string key) override {
-            int pos = h(key);
-            for (V node : table[pos]) {
-                if (node.key == key) {
-                    return node.value;
-                }
-            }
+        table[pos].append(TableEntry<V>(key, value));
+        n++;
+    }
+
+    V search(const std::string &key) override{
+        int pos = h(key);
+        int idx = table[pos].search(TableEntry<V>(key));
+        if (idx == -1) {
             throw std::runtime_error("Key does not exist.");
         }
-        V remove(std::string key) override {
-            int pos = h(key);
-            ListLinked<TableEntry<V>>::remove(pos);
-            n--;
-        }
-        int entries() override {
-            return n;
-        }
+        return table[pos].get(idx).value;
+    }
 
-        
+    V remove(const std::string &key) override{
+        int pos = h(key);
+        int idx = table[pos].search(TableEntry<V>(key));
+        if (idx == -1) {
+            throw std::runtime_error("Key does not exist.");
+        }
+        V value = table[pos].get(idx).value;
+        table[pos].remove(idx);
+        n--;
+        return value;
+    }
+
+    int entries() override{ return n; }
 };
 
 #endif
--- Makefile
+++ Makefile
@@ -1,6 +1,10 @@
 bin/testTableEntry: testTableEntry.cpp TableEntry.h
 	mkdir -p bin
-	g++ -o bin/testTableEntry testTableEntry.cpp
+	g++ -std=c++11 -o bin/testTableEntry testTableEntry.cpp
+
+bin/testHashTable: testHashTable.cpp Dict.h TableEntry.h
+	mkdir -p bin
+	g++ -std=c++11 -o bin/testHashTable testHashTable.cpp
 
 clean:
 	rm -rf *.o *.gch bin
